9b2b4a52a2ab238df65b20188ab82ae41aec8097
Simone PriMaL Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-05-25 16:45:37 +0200

    get character

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataServiceService } from './services/data-service/data-service.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private dataService: DataServiceService) {
+
+    dataService.getRickMortyAPI().subscribe({
+      next: rickmortyapi => console.log('Rick & Morty API:', rickmortyapi),
+      error: err => console.log(err)
+    })
+
+
+    // CHARACTER
+    dataService.getCharacter().subscribe({
+      next: character => console.log('Personaggi:', character),
+      error: err => console.log(err)
+    })
+
+    dataService.getNameCharacter().subscribe({
+      next: nameCharacter => console.log('Nomi Personaggi:', nameCharacter),
+      error: err => console.log(err)
+    })
+
+
+
+
+
+
+
+
+
+  }
+
   title = 'rick-morty-viewer';
 }
--- src/app/services/data-service/data-service.service.ts
+++ src/app/services/data-service/data-service.service.ts
@@ -1,13 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataServiceService {
 
-  constructor() { }
+  readonly RICK_MORTY_URL = 'https://rickandmortyapi.com/api'
+  readonly CHARACTER_URL = 'https://rickandmortyapi.com/api/character'
+  readonly LOCATION_URL = 'https://rickandmortyapi.com/api/location'
+  readonly EPISODE_URL = 'https://rickandmortyapi.com/api/episode'
+
+  constructor(private http:HttpClient) {
+
+    this.getRickMortyAPI();
+    this.getCharacter();
+    this.getNameCharacter();
+    // this.getLocation();
+    // this.getEpisode();
+
+
+  }
+
+  getRickMortyAPI():Observable<any>{
+  return this.http.get<any>(this.RICK_MORTY_URL);
+  }
+
+    // CHARACTER
+  getCharacter():Observable<any>{
+    return this.http.get<any>(this.CHARACTER_URL);
+  }
+
+  getNameCharacter():Observable<any>{
+    return this.http.get<any>(this.CHARACTER_URL).pipe(
+      switchMap((name)=>{
+        const nomeCharacter = name.nome
+        return this.http.get(nomeCharacter)}))
+  }
 
 
 
-  
+
+
+
 }
