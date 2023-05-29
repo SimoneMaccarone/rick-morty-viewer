5913a419ce4f3d16adeff3128c100215fa7251fe
Simone PriMaL Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-05-29 23:25:00 +0200

    joioij

--- src/app/components/personaggi/character/character.component.ts
+++ src/app/components/personaggi/character/character.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { CharacterModel } from 'src/app/model/character-model';
+import { CharacterModel, CharacterResult } from 'src/app/model/character-model';
 import { DataServiceService } from 'src/app/services/data-service/data-service.service';
 
 @Component({
