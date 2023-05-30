aca4b7d04e1d5aee9736f4d543db94888acd6c05
Simone PriMaL Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-05-30 18:08:40 +0200

    making routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, provideRouter, withComponentInputBinding } from '@angular/router';
+import { CharacterPageComponent } from './components/personaggi/character-page/character-page.component';
+import { LocationPageComponent } from './components/luoghi/location-page/location-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'character-page', component: CharacterPageComponent  },
+  { path: 'location-page', component: LocationPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+  ]
 })
 export class AppRoutingModule { }
