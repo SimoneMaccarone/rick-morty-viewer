67b11120bf7ffa5792534262d22a9d19c8ddd60c
Simone PriMaL Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-06-18 23:09:40 +0200

    display episode

--- src/app/components/episodi/episode/episode.component.ts
+++ src/app/components/episodi/episode/episode.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { EpisodeResult } from 'src/app/model/episode-model';
 
 @Component({
   selector: 'app-episode',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class EpisodeComponent {
 
+  @Input() episode?: EpisodeResult
 }
