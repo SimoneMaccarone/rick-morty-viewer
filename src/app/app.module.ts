389a766c90a56d001f13c3590213319017bdcd3f
Simone PriMaL Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-06-21 11:04:47 +0200

    another try pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 
 import { CarouselModule } from '@coreui/angular';
@@ -55,7 +56,8 @@ import { IconModule } from '@coreui/icons-angular';
     ReactiveFormsModule,
     FormsModule,
     IconModule,
-    MatIconModule
+    MatIconModule,
+    MatPaginatorModule
 
 
   ],
