a3ea07b0abd049261e4ca14c413452ac3b5bba77
Simone PriMaL Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-06-27 12:21:37 +0200

    1

--- src/app/services/data-service/data-service.service.ts
+++ src/app/services/data-service/data-service.service.ts
@@ -30,7 +30,7 @@ export class DataServiceService {
 
   // CHARACTER
   getCharacter(): Observable<CharacterResult[]> {
-    return this.http.get<any>(this.CHARACTER_URL + '?page='+ '2').pipe(
+    return this.http.get<any>(this.CHARACTER_URL + '?page='+1).pipe(
       switchMap(character => {
         const results = character.results;
         const getArray = [];
